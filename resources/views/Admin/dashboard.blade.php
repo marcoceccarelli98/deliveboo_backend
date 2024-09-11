b2f92522f1892ff2e260286128ab13ba2b44e1fc
Nicola-Lippo <nicolalippo8991@gmail.com>
2024-09-11 13:07:24 +0200

    UPD Style page admine 4

--- resources/views/Admin/dashboard.blade.php
+++ resources/views/Admin/dashboard.blade.php
@@ -3,30 +3,32 @@
 @section('content')
     <div class="container text-center">
         <h1>DASHBOARD</h1>
-        <div class="py-3">
+        <div class="d-flex justify-content-evenly">
             <a href="{{ route('restaurant.edit') }}" class="btn btn-warning">
                 Modifica Ristorante
             </a>
 
             <a href="{{ route('dishes.create') }}" class="btn btn-success">
-                Aggiungi Nuovo Piatto
+                Aggiungi Piatto
             </a>
         </div>
 
         @if ($dishes->isEmpty())
             <p>Non ci sono piatti disponibili.</p>
         @else
-            <div class="d-flex">
+            <div class="d-flex flex-column">
                 <div class="row">
                     @foreach ($dishes as $dish)
-                        <div class="col-4 py-3">
-                            <div class="card bg-dark-subtle">
+                        <div class="col py-3">
+                            <div class="card bg-dark-subtle h-100">
                                 {{-- <img src="..." class="card-img-top" alt="..."> --}}
                                 <div class="card-body">
                                     <h5 class="card-title">{{ $dish->name }}</h5>
                                     <h6 class="card-text">{{ $dish->price }} €</h6>
-                                    <a href="{{ route('dishes.edit', $dish->slug) }}" class="btn btn-primary"><i
-                                            class="fa-solid fa-pen"></i></a>
+                                    <div class="d-flex justify-content-around">
+                                        <a href="{{ route('dishes.edit', $dish->slug) }}" class="btn btn-primary"><i
+                                                class="fa-solid fa-pen"></i></a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
