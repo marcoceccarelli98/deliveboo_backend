a2aec8f938483a5bb94dbf890528f5bc1cbdcf09
AndVig <andrea.vigna2@gmail.com>
2024-09-12 11:21:35 +0200

    FIX add margins in register form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,11 +11,11 @@
                         <form method="POST" action="{{ route('register') }}" enctype="multipart/form-data">
                             @csrf
 
-                            <div class="form-group row">
+                            <div class="form-group row my-2">
                                 <label for="name"
                                     class="col-md-4 col-form-label text-md-right">{{ __('Nome') }}</label>
 
-                                <div class="col-md-6">
+                                <div class="col-md-6 my-2">
                                     <input id="name" type="text"
                                         class="form-control @error('name') is-invalid @enderror" name="name"
                                         value="{{ old('name') }}" required autocomplete="name" autofocus>
@@ -30,9 +30,9 @@
 
                             <div class="form-group row">
                                 <label for="email"
-                                    class="col-md-4 col-form-label text-md-right">{{ __('Indirizzo E-Mail') }}</label>
+                                    class="col-md-4 col-form-label text-md-right my-2">{{ __('Indirizzo E-Mail') }}</label>
 
-                                <div class="col-md-6">
+                                <div class="col-md-6 my-2">
                                     <input id="email" type="email"
                                         class="form-control @error('email') is-invalid @enderror" name="email"
                                         value="{{ old('email') }}" required autocomplete="email">
@@ -45,11 +45,11 @@
                                 </div>
                             </div>
 
-                            <div class="form-group row">
+                            <div class="form-group row my-2">
                                 <label for="password"
                                     class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
-                                <div class="col-md-6">
+                                <div class="col-md-6 my-2">
                                     <input id="password" type="password"
                                         class="form-control @error('password') is-invalid @enderror" name="password"
                                         required autocomplete="new-password">
@@ -62,22 +62,22 @@
                                 </div>
                             </div>
 
-                            <div class="form-group row">
+                            <div class="form-group row my-2">
                                 <label for="password-confirm"
                                     class="col-md-4 col-form-label text-md-right">{{ __('Conferma Password') }}</label>
 
-                                <div class="col-md-6">
+                                <div class="col-md-6 my-2">
                                     <input id="password-confirm" type="password" class="form-control"
                                         name="password_confirmation" required autocomplete="new-password">
                                 </div>
                             </div>
 
                             {{-- COMPANY NAME --}}
-                            <div class="form-group row">
+                            <div class="form-group row my-2">
                                 <label for="companyName"
                                     class="col-md-4 col-form-label text-md-right">{{ __('Nome Ristorante') }}</label>
 
-                                <div class="col-md-6">
+                                <div class="col-md-6 my-2">
                                     <input id="companyName" type="text"
                                         class="form-control @error('companyName') is-invalid @enderror" name="companyName"
                                         value="{{ old('companyName') }}" required maxlength="20">
@@ -91,11 +91,11 @@
                             </div>
 
                             {{-- CITY --}}
-                            <div class="form-group row">
+                            <div class="form-group row my-2">
                                 <label for="city"
                                     class="col-md-4 col-form-label text-md-right">{{ __('Città') }}</label>
 
-                                <div class="col-md-6">
+                                <div class="col-md-6 my-2">
                                     <input id="city" type="text"
                                         class="form-control @error('city') is-invalid @enderror" name="city"
                                         value="{{ old('city') }}" required maxlength="20">
@@ -109,11 +109,11 @@
                             </div>
 
                             {{-- ADDRESS --}}
-                            <div class="form-group row">
+                            <div class="form-group row my-2">
                                 <label for="address"
                                     class="col-md-4 col-form-label text-md-right">{{ __('Indirizzo') }}</label>
 
-                                <div class="col-md-6">
+                                <div class="col-md-6 my-2">
                                     <input id="address" type="text"
                                         class="form-control @error('address') is-invalid @enderror" name="address"
                                         value="{{ old('address') }}" required maxlength="30">
@@ -127,11 +127,11 @@
                             </div>
 
                             {{-- PIVA --}}
-                            <div class="form-group row">
+                            <div class="form-group row my-2">
                                 <label for="pIva"
                                     class="col-md-4 col-form-label text-md-right">{{ __('P.IVA') }}</label>
 
-                                <div class="col-md-6">
+                                <div class="col-md-6 my-2">
                                     <input id="pIva" type="number"
                                         class="form-control @error('pIva') is-invalid @enderror" name="pIva"
                                         value="{{ old('pIva') }}" required minlength="11" maxlength="11"
@@ -146,13 +146,13 @@
                             </div>
 
                             {{-- IMAGE --}}
-                            <div class="form-group">
+                            <div class="form-group my-2">
                                 <label for="image">Immagine del ristorante</label>
                                 <input type="file" name="image" class="form-control">
                             </div>
 
                             
-                            <div class="mt-5  form-group">
+                            <div class="mt-5  form-group my-2">
                                 <label for="types">Tipologie : </label>
                                 @foreach ($types as $type)
                                 <div class="form-check form-check-inline">
@@ -162,7 +162,7 @@
                                     <label class="btn " for="type-{{ $type->id }}">{{ $type->name }}</label>
                                 </div>
                                 @endforeach
-                                <div class="form-group row mb-0">
+                                <div class="form-group row mb-0 my-2">
                                     <div class="col-md-8 offset-md-4 my-2">
                                         <button type="submit" class="btn btn-primary">
                                             {{ __('Registrati') }}
